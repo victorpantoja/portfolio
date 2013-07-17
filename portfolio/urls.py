8b7568b5fbbda59c85f1a6c135bc63572226d9e0
Victor Pantoja <victor.pantoja@gmail.com>
2013-07-17 18:04:57 -0300

    Making sure index.html will work

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,8 +4,6 @@ from tornado.web import URLSpec
 from portfolio.handlers.page import PageHandler
 
 urls = (
-    URLSpec(r'/', PageHandler, name='home'),
-    URLSpec(r'/(?P<context>.[\w-]+)', PageHandler, name='about'),
-    URLSpec(r'/publications', PageHandler, name='publications'),
-    URLSpec(r'/work', PageHandler, name='work'),
+    URLSpec(r'/(?P<page>index.html)?', PageHandler, name='home'),
+    URLSpec(r'/(?P<context>.[\w-]+)(?P<page>/index.html)?', PageHandler, name='context'),
 )
