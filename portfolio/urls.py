756a9647d48d0abc3a44a3c837b20b84d91cde62
Victor Pantoja <victor.pantoja@gmail.com>
2013-07-19 17:52:33 -0300

    bars at the end

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,5 +5,7 @@ from portfolio.handlers.page import PageHandler
 
 urls = (
     URLSpec(r'/(?P<page>index.html)?', PageHandler, name='home'),
-    URLSpec(r'/(?P<context>.[\w-]+)(?P<page>/index.html)?', PageHandler, name='context'),
+    URLSpec(r'/(?P<context>.[\w-]+)/?', PageHandler, name='context_with_bar'),
+    URLSpec(r'/(?P<context>.[\w-]+)(?P<page>/index.html)?', PageHandler,
+            name='context'),
 )
